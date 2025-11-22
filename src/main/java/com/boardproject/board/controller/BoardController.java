749b15ef24712251f7d3fb99bc516deff3a10786
yoonui <yoony0612@naver.com>
2025-11-22 14:59:45 +0900

    500 에러 해결(@PathVariable 이름 매핑이 깨짐)

--- src/main/java/com/boardproject/board/controller/BoardController.java
+++ src/main/java/com/boardproject/board/controller/BoardController.java
@@ -46,18 +46,20 @@ public class BoardController {
     }
     
     @GetMapping("/{id}")
-    public String findById(@PathVariable Long id, Model model) {
+    public String findById(@PathVariable("id") Long id, Model model
+    ) {
         // 해당 게시글의 조회수를 하나 올림
         // 게시글 데이터를 가져와서 detail.html에 출력
         boardService.updateHits(id);
         BoardDTO boardDTO = boardService.findById(id);
         
         model.addAttribute("board", boardDTO);
+        // model.addAttribute("page", pageable.getPageNumber());
         return "detail";
     }
     
     @GetMapping("/update/{id}")
-    public String updateForm(@PathVariable Long id, Model model) {
+    public String updateForm(@PathVariable("id") Long id, Model model) {
         BoardDTO boardDTO = boardService.findById(id);
         model.addAttribute("boardUpdate", boardDTO);
 
@@ -72,7 +74,7 @@ public class BoardController {
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Long id) {
+    public String delete(@PathVariable("id") Long id) {
         boardService.delete(id);
         return "redirect:/board/";
     }
